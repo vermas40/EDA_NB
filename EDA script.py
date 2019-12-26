e29a3660cd72f76d57974cc64c21902f297839fe
Shivam Verma <Shivam.Verma@mu-sigma.com>
2019-12-26 23:37:23 +0530

    modularized everything and added PCA functions

---
+++ EDA script.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import seaborn as sns
+import time
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder
+from sklearn.model_selection import train_test_split
+
+sns.set()
+
+# Graphics in retina format are more sharp and legible
+#%config InlineBackend.figure_format = 'retina' 
+
+# Increase the default plot size and set the color scheme
+plt.rcParams['figure.figsize'] = 8, 5
+plt.rcParams['image.cmap'] = 'viridis'
+
+os.chdir('C:\\Users\\shivam.verma\\Documents\\Side Hoes\\EDA_NB')
+
+dataset = pd.read_csv('Crashes_Last_Five_Years.csv')
+
+def num_cat(df):
+    num = df.select_dtypes(include=[np.number]).columns
+    cat = list(set(df.columns) - set(num))
+    
+    datatypes = {
+        'numeric' : num,
+        'categorical' : cat
+    }
+    
+    return datatypes
+
+def uni_bi_numeric(df,data_types,lower_threshold = 0.8):
+    print(df[data_types['numeric']].describe())
+    df = df[data_types['numeric']]\
+        .corr()\
+        .unstack()\
+        .to_frame()
+    
+    
+    df = df \
+        .loc[(df[0] > lower_threshold) & (df[0] < 1),]\
+        .sort_values(by=0)\
+        .drop_duplicates(subset = 0,keep = 'first')\
+        .reset_index()
+    
+    df['rower'] = range(1,len(df)+1)
+    
+    return (df)    
+    
+
+def plot_graph(main_dataset,x,type_of_graph):
+    """
+    Earlier call was plot_graph(main_dataset,figure_obj,x,type_of_graph,plotting_dataset = [1])
+    """
+    if type_of_graph == 'scatter':
+        #ax = figure_obj.add_subplot(len(plotting_dataset),1,x[2])
+        plt.scatter(main_dataset[x[0]],main_dataset[x[1]])
+    elif type_of_graph == 'line':
+        #ax = figure_obj.add_subplot(len(plotting_dataset),1,1)
+        plt.plot(main_dataset[x[0]],main_dataset[x[1]])
+    plt.xlabel(x[0])
+    plt.ylabel(x[1])
+    path = os.getcwd()
+    plt.savefig(path + '/Plots/'+ type_of_graph + '/' + x[0]+' vs '+x[1]+'.png', bbox_inches='tight')
+    
+def PCA_func(pca_dataset,target_variable):
+    X = pca_dataset.loc[:,pca_dataset.columns != target_variable]
+    #centering data around 0 and using train data attributes to scale test data as well
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
+    
+    pca = PCA()
+    X = pca.fit_transform(X)
+    
+    explained_variance = pd.DataFrame(pca.explained_variance_ratio_)
+    explained_variance['comp_num'] = range(1,len(explained_variance)+1)
+    explained_variance = explained_variance.rename(columns = {0:'var_explained'})
+    explained_variance['cum_var'] = explained_variance.loc[:,'var_explained'].cumsum(axis = 0)
+    print('Printing Variance Explained vs # of components')
+    plot_graph(explained_variance,['comp_num','cum_var'],'line')
+    return(X)
+    
+def outlier_detection(out_ds,data_types):
+    out_ds.fillna(0,inplace=True)    
+    clf = LocalOutlierFactor(n_neighbors=20)
+    clf.fit_predict(out_ds[data_types['numeric']])
+    X_scores = clf.negative_outlier_factor_
+    out_ds = out_ds.iloc[~out_ds.index.isin(np.where(np.absolute(np.around(X_scores,decimals = 0)) != 1))]
+    return(out_ds)
+    
+
+    
+    
+data_cache = num_cat(dataset)
+plot_dataset = uni_bi_numeric(dataset,data_cache,0.7)
+plot_dataset.loc[:,['level_0','level_1','rower']].apply(lambda x_send: plot_graph(dataset,x_send,'scatter'),axis=1)
+
+dataset_outlier_removed = outlier_detection(dataset,data_cache)
+pca_func(dataset_outlier_removed,target)
+#it works with 20 nearest neighbors
+
+
+
+dummied_variables = pd.get_dummies(dataset[data_cache['categorical']],drop_first=True)
+dataset.drop(columns = data_cache['categorical'],inplace = True)
+
+
+    
+
