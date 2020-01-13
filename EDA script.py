5b5cb170abc6d61287ea44bdec8af7b5f4a5fa0f
vermas40 <mailshivam.verma@gmail.com>
2020-01-13 09:54:43 +0530

    plotting not working

--- EDA script.py
+++ EDA script.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+from scipy.stats import gaussian_kde
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.impute import KNNImputer
 from sklearn.decomposition import PCA,TruncatedSVD
@@ -13,11 +14,7 @@ from sklearn.linear_model import LinearRegression,LogisticRegression
 # Graphics in retina format are more sharp and legible
 #%config InlineBackend.figure_format = 'retina' 
 
-# Increase the default plot size and set the color scheme
-plt.rcParams['figure.figsize'] = 8, 5
-plt.rcParams['image.cmap'] = 'viridis'
-
-os.chdir('C:\\Users\\shivam.verma\\Documents\\Side Hoes\\EDA_NB')
+os.chdir('E:\\Libraries\\Documents\\Side Hoes\\EDA\\EDA\\EDA_NB')
 
 dataset = pd.read_csv('Crashes_Last_Five_Years.csv')
 
@@ -49,20 +46,41 @@ def uni_bi_numeric(df,data_types,lower_threshold = 0.8):
     return (df)    
     
 
-def plot_graph(main_dataset,x,type_of_graph):
+def plot_graph(main_dataset,axis_label,type_of_graph):
     """
     Earlier call was plot_graph(main_dataset,figure_obj,x,type_of_graph,plotting_dataset = [1])
     """
     if type_of_graph == 'scatter':
         #ax = figure_obj.add_subplot(len(plotting_dataset),1,x[2])
         plt.scatter(main_dataset[x[0]],main_dataset[x[1]])
+        plt.xlabel(x[0])
+        plt.ylabel(x[1])
+        figure_name = x[0]+' vs '+x[1]
     elif type_of_graph == 'line':
         #ax = figure_obj.add_subplot(len(plotting_dataset),1,1)
         plt.plot(main_dataset[x[0]],main_dataset[x[1]])
-    plt.xlabel(x[0])
-    plt.ylabel(x[1])
+        plt.xlabel(x[0])
+        plt.ylabel(x[1])
+        figure_name = x[0]+' vs '+x[1]
+    elif type_of_graph == 'before_outlier_density':
+        figure_name = main_dataset.name
+        main_dataset.plot(kind='density')
+        plt.xlabel(x[0])
+        plt.ylabel(x[1]) 
+
+        
+    elif type_of_graph == 'after_outlier_density':
+        density = gaussian_kde(main_dataset)
+        output_axis = np.linspace(min(main_dataset),max(main_dataset),100)
+        plt.plot(output_axis,density(output_axis))
+        plt.xlabel(axis_label[0])
+        plt.ylabel(axis_label[1])        
+        figure_name = main_dataset.name
+        
+
     path = os.getcwd()
-    plt.savefig(path + '/Plots/'+ type_of_graph + '/' + x[0]+' vs '+x[1]+'.png', bbox_inches='tight')
+    plt.savefig(path + '/Plots/'+ type_of_graph + '/' + figure_name +'.png', bbox_inches='tight')
+    
     return(1)
     
 def PCA_func(df,type_PCA):
@@ -149,7 +167,7 @@ def feature_selection(df,target,exercise):
     selector = RFE(estimator,step=1)
     selector = selector.fit(X,y)
     
-    
+    pd.DataFrame(X.loc[:,selector.support_].columns,columns = ['imp_features']).to_csv(os.getcwd() + '\Feature_Selection\Imp_features.csv',index=False)
     return(X.loc[:,selector.support_].columns)
 
 target = 'ALCOHOL_RELATED'
@@ -157,10 +175,16 @@ data_cache = num_cat(dataset)
 plot_dataset = uni_bi_numeric(dataset,data_cache,0.9)
 plot_dataset.loc[:,['level_0','level_1']].apply(lambda x_send: plot_graph(dataset,x_send,'scatter'),axis=1)
 dataset.loc[:,dataset.columns[dataset.isna().any()]] = dataset.loc[:,dataset.columns[dataset.isna().any()]].apply(missing_value_treatment)
+
+dataset.loc[:,data_cache['numeric']].apply(lambda x: plot_graph(x,['values','probability'],'after_outlier_density'),axis=0)
+for column in data_cache['numeric']:
+    dataset.loc[:,[column]].apply(lambda x: plot_graph(x,['values','probability'],'after_outlier_density'),axis=0)
+
 dataset_outlier_removed = outlier_detection(dataset,data_cache)
+dataset_outlier_removed.loc[:,data_cache['numeric']].apply(lambda x: plot_graph(x,['values','probability'],'after_outlier_density'),axis=0)
 dataset_outlier_removed_hashed_cat = encoding(dataset_outlier_removed.loc[:,set(data_cache['categorical']) - set(target)])
 pca_components_cat_data = PCA_func(dataset_outlier_removed_hashed_cat,'sparse')
 dataset_categorical_pca = pd.concat([dataset_outlier_removed[data_cache['numeric']],pca_components_cat_data,dataset_outlier_removed[target]],axis=1)
-
+imp_features = feature_selection(dataset_categorical_pca,target,'classification')
 
 
